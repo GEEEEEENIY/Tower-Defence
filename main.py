3d5f59529d9bafdff913b5594c9889423bbd6ad9
unknown <kostik.dmitriev08@mail.ru>
2026-03-14 17:24:25 +0300

    Добавил врагов , счёт

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import pygame 
 
+class Sprite:
+    def __init__(self,image,center, ):
+        self.image = image
+        self.rect = self.image.get_frect()
+        self.rect.center = center
+        
+    def render(self):
+        surface.blit(self.image, self.rect)
+
 WINDOW_SIZE = [800, 600 ]
 MAX_FPS = 60
 
 window = pygame.Window('Tower Defence', WINDOW_SIZE)
 surface = window.get_surface()
 clock = pygame.Clock()
+image = pygame.Surface([50,50])
+image.fill('green')
+player = Sprite(image,[WINDOW_SIZE[0] / 2 , WINDOW_SIZE[1] / 1 ])
 
 running = True 
 while running :
@@ -13,7 +25,9 @@ while running :
         if event.type == pygame.WINDOWCLOSE:
             running = False
 
-    surface.fill('black')
-
+    surface.fill('white')
+    player.render()
+    window.flip()
+    
     clock.tick()
     print(clock.get_fps)
